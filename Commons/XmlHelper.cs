097fd925ba7c4b51832e2e41c266838abe0fd91f
oleg3790 <oleg.krysko@outlook.com>
2018-05-11 12:49:05 -0500

    Added additional helper to XmlHelper

--- Commons/XmlHelper.cs
+++ Commons/XmlHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Xml;
+using System.Xml.Serialization;
 
 namespace Commons
 {
@@ -24,6 +26,41 @@ namespace Commons
             {
                 throw;
             }
+        }
+
+        public static XmlElement SerializeToXmlElement( object obj, XmlSerializerNamespaces ns )
+        {
+            XmlDocument doc = new XmlDocument();
+
+            using (XmlWriter writer = doc.CreateNavigator().AppendChild())
+            {
+                new XmlSerializer(obj.GetType()).Serialize(writer, obj, ns);
+            }
+            return doc.DocumentElement;
+        }
+
+        public static XmlDocument SerializeToXmlDocument( object input, string namespaceUri )
+        {
+            XmlSerializer ser = new XmlSerializer(input.GetType(), namespaceUri);
+
+            XmlDocument doc = null;
+
+            using (MemoryStream memStm = new MemoryStream())
+            {
+                ser.Serialize(memStm, input);
+
+                memStm.Position = 0;
+
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.IgnoreWhitespace = true;
+
+                using (var xtr = XmlReader.Create(memStm, settings))
+                {
+                    doc = new XmlDocument();
+                    doc.Load(xtr);
+                }
+            }
+            return doc;
         }
     }
 }
